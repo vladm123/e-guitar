44857ee6f05110f469d6a4e98fc835a8582d6573
Vlad Manea <vlad.c.manea@gmail.com>
2013-11-12 08:02:11 -0500

    Updating app.js and package.json

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var projects = require('./routes/projects');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function() {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
+    
+    // For development only
+    if ('development' == app.get('env')) {
+        app.use(express.errorHandler());
+    }
+});
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.get('/', function(req, res) { res.send("OK"); })
 
-app.get('/', routes.index);
+app.get('/projects', projects.selectAll);
+app.get('/projects/:id', projects.selectById);
+app.get('/projectsinsert', projects.insert);
+app.get('/projectsdelete/:id', projects.deleteById);
+// app.put('/projects/:id', projects.updateById);
+// app.delete('/projects/:id', projects.deleteById);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
